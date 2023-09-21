a76c4ef70fac8cbd4ddec83da4323552c7f74318
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2023-09-21 22:08:11 +0530

    Initail Files structure for the Project

---
+++ libranian.java
@@ -0,0 +1,5 @@
+package Library-Management-System.Images;
+
+public class Manager {
+    
+}
---
+++ main.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+//import javax.swing;
+public class main{
+    public static void main(String agrs[]){
+        JFrame
+    }
+}
