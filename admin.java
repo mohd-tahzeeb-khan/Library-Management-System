65d208b1a449afa9eed7953c76a912cf48d2c90b
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2023-09-30 01:05:01 +0530

    working on admin.java file

--- admin.java
+++ admin.java
@@ -1,5 +1,23 @@
-public class admin{
+import java.awt.Color;
+import javax.swing.*;
+public class admin extends JFrame {
+    admin(){
+        JFrame admindashboard =new JFrame("Admin Dashboard");
+        JPanel heading_panel=new JPanel();
+        heading_panel.setBackground(Color.WHITE);
+        heading_panel.setBounds(0,0,1500, 800);
+        JButton logout=new JButton("LOGOUT");
+        logout.setBounds(150,150,100,40);
+        logout.setBackground(Color.RED);
+        logout.setForeground(Color.WHITE);
+        admindashboard.add(heading_panel);
+        heading_panel.add(logout);
+        admindashboard.setVisible(true);
+        admindashboard.setSize(1500, 800);
+        admindashboard.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
     public static void main(String args[]){
-        System.out.println("Tahzeeb hello. This program is up to the mark and running on this pc");
+        admin object=new admin();
     }
 }
