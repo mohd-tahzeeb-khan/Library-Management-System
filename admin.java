b267a2aee6bfa04bd37fa5393a3d334a75257e98
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2023-09-30 17:08:09 +0530

    working on admin dashboard

--- admin.java
+++ admin.java
@@ -1,23 +1,56 @@
+import java.awt.CardLayout;
 import java.awt.Color;
+
 import javax.swing.*;
-public class admin extends JFrame {
+ 
+public class admin extends JFrame{
+    JFrame Admin;
+    JLabel name, logo_images;
+    JButton logout, help;
+    JPanel heading_Panel, plain_Panel, dynamicpanel;
+    CardLayout cardlayoutinstance;
+    ImageIcon icon;
+
     admin(){
-        JFrame admindashboard =new JFrame("Admin Dashboard");
-        JPanel heading_panel=new JPanel();
-        heading_panel.setBackground(Color.WHITE);
-        heading_panel.setBounds(0,0,1500, 800);
-        JButton logout=new JButton("LOGOUT");
-        logout.setBounds(150,150,100,40);
+        Admin=new JFrame();
+        Admin.setTitle("Admin Dashboard");
+        heading_Panel=new JPanel();
+        heading_Panel.setBounds(20,15,1450, 100);
+        heading_Panel.setBackground(Color.LIGHT_GRAY);
+        plain_Panel=new JPanel();
+        plain_Panel.setBounds(20,15,1450, 100);
+        plain_Panel.setBackground(Color.WHITE);
+        Admin.setSize(1500, 800);
+        icon=new ImageIcon("images/logo.png");
+        logo_images=new JLabel(icon);
+        logo_images.setBounds(30,35,80,65);
+        name=new JLabel("Tahzeeb");
+        name.setBounds(150,20,150,60);
+        help=new JButton("HELP");
+        help.setBounds(1350, 30, 100, 35);
+        help.setBackground(Color.LIGHT_GRAY);
+        help.setForeground(Color.BLACK);
+        logout=new JButton("LOGOUT");
+        logout.setBounds(1350, 70, 100, 35);
         logout.setBackground(Color.RED);
         logout.setForeground(Color.WHITE);
-        admindashboard.add(heading_panel);
-        heading_panel.add(logout);
-        admindashboard.setVisible(true);
-        admindashboard.setSize(1500, 800);
-        admindashboard.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+        cardlayoutinstance=new CardLayout();
+        dynamicpanel=new JPanel(cardlayoutinstance);
+        JPanel dashboard=new JPanel();
 
+        Admin.setVisible(true);
+        Admin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        Admin.add(name);
+        Admin.add(logo_images);
+        Admin.add(help);
+        Admin.add(logout);
+        Admin.add(plain_Panel);
+        Admin.add(heading_Panel);
+
+    }
     public static void main(String args[]){
-        admin object=new admin();
+        admin adminobj=new admin();
     }
 }
