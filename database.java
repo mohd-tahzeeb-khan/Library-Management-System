7b467b70b94d71767628084a34b0a17213458d6f
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2023-11-04 02:17:21 +0530

    database file

--- database.java
+++ database.java
@@ -1,5 +1,22 @@
 import java.sql.*;
 public class database{
+    public void insert_bookINTOdb(){
+        Connection c=null;
+        Statement stmt=null;
+        ResultSet rs;
+        try{
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:G:/Java Project/Library-Management-System/Library.db");      
+            stmt=c.createStatement();
+            String sql="INSERT INTO Book VALUES('', '', '');";
+            stmt.executeUpdate(sql);
+            c.close();
+          }
+        catch(Exception e){ 
+            System.err.println(e);
+            System.exit(0);
+        }
+    }
     database(){
         Connection c=null;
         Statement stmt=null;
