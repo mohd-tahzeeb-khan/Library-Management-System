8e7dd8b41ea9f48c332a69bd6a4adf689d332b7a
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2023-11-12 02:43:28 +0530

    working on database file to insert data into the database(actual one)

--- database.java
+++ database.java
@@ -1,43 +1,64 @@
 import java.sql.*;
 public class database{
-    public void insert_bookINTOdb( String bookname, String isssn){
+    public void bookINTOdb( String bookname, String isbn, String author,String publisher,String genre, String price, String publishedyear, String quantity, String langauage, String dateofentry, String format,String edition, String pages, String department ){
         Connection c=null;
         Statement stmt=null;
+        PreparedStatement pst1;
         ResultSet rs;
         try{
             Class.forName("org.sqlite.JDBC");
             c = DriverManager.getConnection("jdbc:sqlite:G:/Java Project/Library-Management-System/Library.db");      
-            stmt=c.createStatement();
-            PreparedStatement sqlquery =null;
-            String sql="INSERT INTO Books VALUES(?,?);";
-            sqlquery.setStrng(1,bookname);
-            sqlquery.setStrng(2,isssn);
-            stmt.executeUpdate(sql);
-            c.close();
+            try{
+                pst1=c.prepareStatement("INSERT INTO Books VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?);");
+                pst1.setString(1, isbn);
+                pst1.setString(2, bookname);
+                pst1.setString(3, author);
+                pst1.setString(4, publisher);
+                pst1.setString(5, genre);
+                pst1.setString(6, price);
+                pst1.setString(7, publishedyear);
+                pst1.setString(8, quantity);
+                pst1.setString(9, langauage);
+                pst1.setString(10, dateofentry);
+                pst1.setString(11, format);
+                pst1.setString(12, edition);
+                pst1.setString(13, pages);
+                pst1.setString(14, department);
+                int i=pst1.executeUpdate();
+                System.out.println(i);
+                c.commit();
+                c.close();
           }
+          catch(Exception E){
+            System.out.print(E);
+          }
+            }
+            
         catch(Exception e){ 
             System.err.println(e);
             System.exit(0);
         }
     }
-    database(){
-        Connection c=null;
-        Statement stmt=null;
-        ResultSet rs;
-        try{
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:G:/Java Project/Library-Management-System/Library.db");      
-            stmt=c.createStatement();
-            String sql="INSERT INTO login VALUES('', '', '', ????????);";
-            stmt.executeUpdate(sql);
-            c.close();
-          }
-        catch(Exception e){ 
-            System.err.println(e);
-            System.exit(0);
-        }
-    }
+    // database(){
+    //     Connection c=null;
+    //     Statement stmt=null;
+    //     ResultSet rs;
+    //     try{
+    //         Class.forName("org.sqlite.JDBC");
+    //         c = DriverManager.getConnection("jdbc:sqlite:G:/Java Project/Library-Management-System/Library.db");      
+    //         stmt=c.createStatement();
+    //         String sql="INSERT INTO login VALUES('', '', '', ????????);";
+    //         stmt.executeUpdate(sql);
+    //         c.close();
+    //       }
+    //     catch(Exception e){ 
+    //         System.err.println(e);
+    //         System.exit(0);
+    //     }
+    // }
     public static void main(String args[]){
         database insert=new database();
+        insert.bookINTOdb("dfgd,","dfgd,","dfgd,","dfgd,","dfgd,","dfgd,","dfgd,","dfgd,","dfgd,","dfgd,","dfgd,","dfgd,","dfgd,","dfgd,");
+        //insert.insert_bookINTOdb("Lets Learn Python", "15785");
     }
 }
